d0d46f65910a4ae60b911cf6c6d094dbbb0a80fe
Kim Mok <cafe.mui@gmail.com>
2016-06-29 17:21:20 -0400

    included react stylesheet

--- propertyFinder/index.ios.js
+++ propertyFinder/index.ios.js
@@ -1,2 +1,14 @@
 'use strict';
+
+//importing react-native module and assigns it to `React`
 var React = require('react-native');
+
+//defined a single style called `text`
+var styles = React.StyleSheet.create({
+  text: {
+    color: 'black',
+    backgroundColor: 'white',
+    fontSize: 30,
+    margin: 80
+  }
+});
